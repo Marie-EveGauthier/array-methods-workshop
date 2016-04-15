13d23b0d4aec59bebfc195aaddbce87096856c5f
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2016-04-15 20:35:16 +0000

    function longestWord

--- answers.js
+++ answers.js
@@ -20,11 +20,47 @@ and uses its filter method to return a new array with only the positive numbers
 */
 
 function getPositives(arr) {
-    arr.filter(function(element){
+    return arr.filter(function(element){
         if(element > 0){
             return element;
         }
     });
 }
+console.log(getPositives([-2, 2, 3, 4, -5, 0, -10]));
 
-getPositives([-2, 2, 3, 4, -5, 0, -10]);
+/*Exercise 3: A function called filterArray 
+that takes an array AND a function as arguments. 
+Your filter function should return a new array that contains only the elements where the passed function returns a truthy value.
+*/
+
+function filterArray (arr, foo) {
+    return arr.filter(foo);
+    }
+
+console.log(filterArray([true, false, "true", "false", 0, -1, 1], 
+function isEven(num){
+    if(num % 2 === 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+));
+
+/* Exercise 4: A function called longestWord that takes a string as argument,
+and returns the longest word in the string. You should use Array.prototype.reduce to do your work.
+*/
+function longestWord(str){
+    var arrOfWord= str.split(" ");
+    return arrOfWord.reduce(function(accumulator, currentValue) {
+        if (currentValue.length > accumulator.length) {
+            accumulator = currentValue;
+        } 
+        return accumulator;
+    }, []);    
+}
+
+
+console.log(longestWord("Javasript est un outil merveilleux."));
+
